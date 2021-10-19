a096ac14e9a8a4fe7c56b70ecfdd7bf3b60d8112
Felipev98 <felipe_v_spd@hotmail.com>
2021-10-19 14:58:11 -0400

    add: new styles

--- js/carrusel.js
+++ js/carrusel.js
@@ -2,10 +2,11 @@
 new Splide( '.splide', {
 	type        : 'loop',
 	autoplay    : true,
-	pauseOnHover: false,
 	lazyLoad: 'nearby',
 	'cover'      : true,
+	'center'     :true,
 	'heightRatio': 0.5,
+	
 
 } ).mount();
 
